acd492898e3654f72f3f7df909b453d8f906297d
Alexandr <89970757+AladinAlex@users.noreply.github.com>
2022-11-15 09:27:46 +0300

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,11 @@
+$(function() {
+	$(".hamburger").click(function() {
+		$(this).toggleClass("is-active");
+		
+		if($(this).hasClass('is-active')){
+			$('.menu_top').slideDown(300);
+		}else{
+			$('.menu_top').slideUp(300);
+		}
+	});
+});
